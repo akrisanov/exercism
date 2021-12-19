cbd3ab95e127ae5a79779a4a288c247ddbdec17c
Andrey Krisanov <develop4net@gmail.com>
2021-12-19 13:29:33 +0300

    Rust track: Review the Assembly Line solution

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,20 +2,23 @@
 // to enable stricter warnings.
 // #![allow(unused)]
 
-pub fn production_rate_per_hour(speed: u8) -> f64 {
-    let cars_per_hour = 221.0;
+const CARS_PER_HOUR: f64 = 221.0;
+const MIN_IN_HOUR: u32 = 60;
 
-    let success_rate = match speed {
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    CARS_PER_HOUR * speed as f64 * production_success_rate(speed)
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    production_rate_per_hour(speed) as u32 / MIN_IN_HOUR
+}
+
+fn production_success_rate(speed: u8) -> f64 {
+    match speed {
         0 => 0.0,
         1..=4 => 1.0,
         5..=8 => 0.9,
         9..=10 => 0.77,
-        _ => std::process::exit(-1),
-    };
-
-    cars_per_hour * speed as f64 * success_rate
-}
-
-pub fn working_items_per_minute(speed: u8) -> u32 {
-    production_rate_per_hour(speed) as u32 / 60
+        _ => panic!("The assembly line's speed can range from 0 (off) to 10 (maximum)."),
+    }
 }
