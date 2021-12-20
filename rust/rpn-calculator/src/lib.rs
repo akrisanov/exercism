5deeed2cde4aa4ee03f0deeba9b31a79c2c2eca0
Andrey Krisanov <develop4net@gmail.com>
2021-12-20 22:43:29 +0300

    Rust track: RPN Calculator

---
+++ rust/rpn-calculator/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, Copy, Clone)]
+pub enum CalculatorInput {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Value(i32),
+}
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    let mut evaluation: Vec<i32> = Vec::new();
+
+    for &input in inputs {
+        match input {
+            CalculatorInput::Value(number) => evaluation.push(number),
+            _ => {
+                let snd = evaluation.pop()?;
+                let fst = evaluation.pop()?;
+
+                match input {
+                    CalculatorInput::Add => evaluation.push(fst + snd),
+                    CalculatorInput::Subtract => evaluation.push(fst - snd),
+                    CalculatorInput::Multiply => evaluation.push(fst * snd),
+                    CalculatorInput::Divide => evaluation.push(fst / snd),
+                    _ => return None,
+                }
+            }
+        }
+    }
+
+    match evaluation.len() {
+        1 => evaluation.pop(),
+        _ => None,
+    }
+}
